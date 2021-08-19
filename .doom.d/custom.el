760d2304d5d8355f32a96a6ca768cd4ad9f0aee0
conorbros <conorbros@protonmail.com>
2021-08-19 19:50:31 +1000

    updates

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -6,7 +6,7 @@
  '(ansi-color-names-vector
    ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
  '(custom-safe-themes
-   '("fe2539ccf78f28c519541e37dc77115c6c7c2efcec18b970b16e4a4d2cd9891d" "f7fed1aadf1967523c120c4c82ea48442a51ac65074ba544a5aefc5af490893b" "4b6b6b0a44a40f3586f0f641c25340718c7c626cbf163a78b5a399fbe0226659" "d47f868fd34613bd1fc11721fe055f26fd163426a299d45ce69bef1f109e1e71" "8d7b028e7b7843ae00498f68fad28f3c6258eda0650fe7e17bfb017d51d0e2a2" "26e07f80888647204145085c4fed78e0e6652901b62a25de2b8372d71de9c0a1" "6b1abd26f3e38be1823bd151a96117b288062c6cde5253823539c6926c3bb178" "266ecb1511fa3513ed7992e6cd461756a895dcc5fef2d378f165fed1c894a78c" "e6a2832325900ae153fd88db2111afac2e20e85278368f76f36da1f4d5a8151e" "b7e460a67bcb6cac0a6aadfdc99bdf8bbfca1393da535d4e8945df0648fa95fb" "6c386d159853b0ee6695b45e64f598ed45bd67c47f671f69100817d7db64724d" "4f1d2476c290eaa5d9ab9d13b60f2c0f1c8fa7703596fa91b235db7f99a9441b" "22a514f7051c7eac7f07112a217772f704531b136f00e2ccfaa2e2a456558d39" "20f786e5d7b19abd8d93579a809976f2e477359a64ffdf36c4a07c6875045ece" default))
+   '("22a514f7051c7eac7f07112a217772f704531b136f00e2ccfaa2e2a456558d39" default))
  '(exwm-floating-border-color "#191b20")
  '(fci-rule-color "#5B6268")
  '(highlight-tail-colors
@@ -18,7 +18,6 @@
  '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#98be65"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
  '(objed-cursor-color "#ff6c6b")
- '(package-selected-packages '(treemacs-all-the-icons))
  '(pdf-view-midnight-colors (cons "#bbc2cf" "#282c34"))
  '(rustic-ansi-faces
    ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
