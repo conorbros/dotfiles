a761ac07186b59b22d61e40c44bd722d68db7195
conorbros <conorbros@protonmail.com>
2021-08-31 20:18:03 +1000

    updates

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias gits='git status'
+alias pacman='sudo pacman'
+alias doom='$HOME/.emacs.d/bin/doom'
+export PATH="$HOME/.cargo/bin:$PATH"
+alias clippy-dirty="$HOME/scripts/clippy-dirty"
+alias ls='lsd'
