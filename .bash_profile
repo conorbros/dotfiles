295a92d58bb78ce820ffe1d631c19ddada801b36
conorbros <conorbros@protonmail.com>
2023-01-18 11:41:31 +1000

    updates

---
+++ .bash_profile
@@ -0,0 +1,8 @@
+#
+# ~/.bash_profile
+#
+
+[[ -f ~/.bashrc ]] && . ~/.bashrc
+
+source /home/conor/.config/broot/launcher/bash/br
+. "$HOME/.cargo/env"
--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("51c71bb27bdab69b505d9bf71c99864051b37ac3de531d91fdad1598ad247138" "2e05569868dc11a52b08926b4c1a27da77580daa9321773d92822f7a639956ce" default))
  '(package-selected-packages '(toml-mode treemacs-all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
